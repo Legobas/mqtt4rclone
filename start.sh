f431385a7816227292e22c33e742c9befad68187
bdr <legobas@gmail.com>
2025-07-12 20:14:54 +0200

    skip --log-format

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 usermod -o -u "$PUID" -g "$PGID" appuser
 su -s /bin/bash appuser -c \
-'/usr/bin/multirun "/usr/bin/rclone rcd --config /config/rclone.conf --log-format none --log-level '"$RCLONE_LOGLEVEL"' --rc-web-gui-no-open-browser --rc-user cmd --rc-pass f6Lhi09wfbxkd8Ok2l4H" "/bin/app"'
+'/usr/bin/multirun "/usr/bin/rclone rcd --config /config/rclone.conf --log-level '"$RCLONE_LOGLEVEL"' --rc-web-gui-no-open-browser --rc-user cmd --rc-pass f6Lhi09wfbxkd8Ok2l4H" "/bin/app"'
